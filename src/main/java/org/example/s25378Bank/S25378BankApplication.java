b267f1fcdeda3b1f873dc97d8bc3f3afcb919437
mvr3k <61803073+mvr33k@users.noreply.github.com>
2024-01-27 12:06:28 +0100

    Initial commit

---
+++ src/main/java/org/example/s25378Bank/S25378BankApplication.java
@@ -0,0 +1,13 @@
+package org.example.s25378Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S25378BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S25378BankApplication.class, args);
+	}
+
+}
